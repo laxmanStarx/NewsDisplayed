18d5290d81bf46064d18e1a4f62f421bdefe319c
laxmanStarx <123666392+laxmanStarx@users.noreply.github.com>
2023-10-23 23:05:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -38,10 +38,10 @@ export default class App extends Component {
         
       />
         <Routes>
-          <Route exact path='/' element= { <News  setProgress={this.setProgress} apiKey={this.apiKey}key="general" pageSize={this.pageSize} country="in" category="general"/> }/>
+          <Route exact path='/' element= { <News  setProgress={this.setProgress} apiKey={this.apiKey}key="general" pageSize={this.pageSize} country="us" category="general"/> }/>
           <Route exact path='/entertainment' element= { <News  setProgress={this.setProgress}apiKey={this.apiKey}key="entertainment" pageSize={this.pageSize} country="us" category="entertainment"/> }/>
           <Route exact path='/business' element= { <News  setProgress={this.setProgress}apiKey={this.apiKey}key="business" pageSize={this.pageSize} country="us" category="business"/> }/>
-          <Route exact path='/health' element= { <News  setProgress={this.setProgress}apiKey={this.apiKey}key="health" pageSize={this.pageSize} country="in" category="health"/> }/>
+          <Route exact path='/health' element= { <News  setProgress={this.setProgress}apiKey={this.apiKey}key="health" pageSize={this.pageSize} country="us" category="health"/> }/>
           {/* <Route exact path='/' element= { <News  setProgress={this.setProgress}apiKey={this.apiKey}"general" pageSize={this.pageSize} country="in" category="general"/> }/> */}
           <Route exact path='/science' element= { <News  setProgress={this.setProgress}apiKey={this.apiKey}key="science" pageSize={this.pageSize} country="us" category="science"/> }/>
           <Route exact path='/sports' element= { <News  setProgress={this.setProgress}apiKey={this.apiKey}key="sports" pageSize={this.pageSize} country="us" category="sports"/> }/>
